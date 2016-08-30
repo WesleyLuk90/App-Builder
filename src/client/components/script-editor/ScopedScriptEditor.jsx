4d2a77008b24c58154645b80854c6ed64658fccf
Wesley Luk <wesleyluk@gmail.com>
2016-08-29 19:23:52 -0700

    Variable Editor Styling

--- src/client/components/script-editor/ScopedScriptEditor.jsx
+++ src/client/components/script-editor/ScopedScriptEditor.jsx
@@ -16,20 +16,42 @@ export default class ScopedScriptEditor extends React.Component {
 		return this.getProgramBuilder().getScopedProgramBuilders();
 	}
 
+	hasChildrenScopes() {
+		return this.getScopedProgramBuilders().length > 0;
+	}
+
 	getChildScopeProps(programBuilder) {
 		return Object.assign({}, this.props, { programBuilder });
 	}
 
 	render() {
 		return (<div className="script-editor">
-			<button className="button">Add Variable</button>
-			<div className="variable-list">
-				{this.getLocalVariables().map((variableBuilder, index) => <VariableEditor key={index} variableBuilder={variableBuilder} {...this.props} />)}
+			<div className="script-editor__header">
+				<h2><a><span className="fa fa-plus-square-o" /> Scope {this.getProgramBuilder().getScopePath().toString()}</a></h2>
+				<button className="button">Add Variable</button>
 			</div>
-			<div className="scope-list">
-				{this.getScopedProgramBuilders().map((scopedProgramBuilder, index) =>
-					<ScopedScriptEditor key={index} {...this.getChildScopeProps(scopedProgramBuilder)} />
-				)}
+			<div className="script-editor__scope">
+				<h3>Variables</h3>
+				<div className="variable-list">
+					<div className="variable-list__header">
+						<div className="variable-list__header__name">Variable</div>
+						<div className="variable-list__header__type">Data Type</div>
+						<div className="variable-list__header__variable-type">Data Source</div>
+					</div>
+					{this.getLocalVariables().map((variableBuilder, index) => <VariableEditor key={index} variableBuilder={variableBuilder} {...this.props} />)}
+				</div>
+				{this.hasChildrenScopes() ?
+					<div>
+						<h3>Child Scopes</h3>
+						<div className="scope-list">
+							{this.getScopedProgramBuilders().map((scopedProgramBuilder, index) =>
+								<ScopedScriptEditor key={index} {...this.getChildScopeProps(scopedProgramBuilder)} />
+							)}
+						</div>
+					</div>
+					:
+					null
+				}
 			</div>
 		</div>);
 	}
